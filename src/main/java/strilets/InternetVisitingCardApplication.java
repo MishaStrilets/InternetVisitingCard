218e439cd3c89c8e3545bf268dbcd9cd9a759f09
Misha Strilets <mishastrilets@gmail.com>
2017-08-07 23:52:32 +0300

    create project

---
+++ src/main/java/strilets/InternetVisitingCardApplication.java
@@ -0,0 +1,12 @@
+package strilets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetVisitingCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetVisitingCardApplication.class, args);
+	}
+}
